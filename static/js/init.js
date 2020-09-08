d1b799480284d2e4e44e1659a4b62882c648aa9d
SylleoYr <23133978+SylleoYr@users.noreply.github.com>
2020-09-08 16:58:10 +0800

    FEATURE: custom hitokoto type

--- static/js/init.js
+++ static/js/init.js
@@ -1,22 +1,26 @@
-function hitokoto() {
+function hitokoto(hiType) {
     if (hitokoto_timer) {
         clearTimeout(hitokoto_timer);
     }
+
     hitokoto_timer = setTimeout(function () {
-        fetch('https://v1.hitokoto.cn?c=i&encode=json&charset=utf-8')
+        fetch('https://v1.hitokoto.cn?encode=json&charset=utf-8&' + hiType)
         .then(response => response.json())
         .then(data => {
             const hi = document.getElementById("hitokoto");
             hi.innerText = data.hitokoto;
+
+            const hi_from = document.getElementById("hitokoto-from");
+            hi_from.innerText = "—— " + data.from_who + " ·《"+ data.from + "》";
         })
     }, 256);
 }
 
 var hitokoto_timer;
 
-function init() {
-    hitokoto();
-    setInterval(hitokoto, 300000);
+function init(hiType) {
+    hitokoto(hiType);
+    setInterval(hitokoto, 300000, hiType);
 
     var firstSearchGroup = document.getElementsByClassName("search-group-list")[0].firstElementChild;
     changeSearchGroup(firstSearchGroup.innerText.trim());
