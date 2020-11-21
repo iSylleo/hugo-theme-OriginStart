6faf27ac044869d49b23fc26479500844d5d1fb0
SylleoYr <SylleoYr@users.noreply.github.com>
2020-11-21 08:58:18 +0000

    deploy: 5241e0ab4f0590192619f66cac0d0f7db9bdfa9f

--- js/alert.js
+++ js/alert.js
@@ -1,1 +1,1 @@
-alert("This webpage is only for project display, the relevant links are randomly selected examples, please do not abuse.\n该网页仅作项目展示，相关链接均为随机选取的示例，请勿滥用。")
+alert("This webpage is only for project display, the relevant links are randomly selected examples, please do not abuse.\n该网页仅作项目展示，相关链接均为随机选取的示例，请勿用作非法用途。")
